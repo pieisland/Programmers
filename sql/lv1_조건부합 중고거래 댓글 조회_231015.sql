a9480998adb2e589dad26e3412fed540f0f4da2a
pieisland <35261724+pieisland@users.noreply.github.com>
2023-10-15 18:46:44 +0900

    Create lv1_조건부합 중고거래 댓글 조회_231015.sql

---
+++ sql/lv1_조건부합 중고거래 댓글 조회_231015.sql
@@ -0,0 +1,6 @@
+select a.title, a.board_id, b.reply_id, b.writer_id, b.contents,
+date_format(b.created_date, '%Y-%m-%d') as created_date
+from used_goods_board a right join used_goods_reply b
+on a.board_id = b.board_id
+where a.created_date like '2022-10%'
+order by b.created_date, a.title
